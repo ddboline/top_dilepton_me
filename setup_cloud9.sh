f9282d3bd8b13cd4f9397d6625256f4e82d8881e
Daniel Boline <daniel.boline@stonybrook.edu>
2014-11-25 21:46:14 -0500

    gsl, pdf

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install -y gfortran libroot-bindings-python-dev libroot-graf2d-postscript-dev libroot-core-dev libroot-math-physics-dev
-sudo apt-get install -y libgsl0-dev liblhapdf-dev libroot-graf2d-postscript-dev
+sudo apt-get install -y libgsl0-dev liblhapdf-dev
 mkdir -p obj/
