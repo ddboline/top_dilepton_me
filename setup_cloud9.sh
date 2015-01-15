cfe160ca41f720f9919ef67f1f829a2f0dda0f14
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-15 17:06:00 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install -y gfortran libroot-bindings-python-dev libroot-graf2d-postscript-dev libroot-core-dev libroot-math-physics-dev
-sudo apt-get install -y libgsl0-dev liblhapdf-dev
+sudo apt-get install -y libgsl0-dev liblhapdf-dev make
 mkdir -p obj/
+make -f bin/Makefile
